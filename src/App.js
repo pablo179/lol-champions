7ebfe49b019e1b01ce44e516a668ac1aef56ef6d
pablo179 <cm.pablo.prog@gmail.com>
2022-02-12 11:25:18 -0600

    this can build

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from 'react';
 import './App.css';
-import Routes from './Routes';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  componentWillMount() {
-    const getChampions = async ()  => {
-      const championsRequest = await axios.get('http://ddragon.leagueoflegends.com/cdn/11.6.1/data/es_MX/champion.json');
-      const keys = Object.keys(championsRequest.data.data);
-      const newArrayChampions = {}
-      keys.forEach((item)=> {
-        newArrayChampions[item.toLowerCase()] = championsRequest.data.data[item]
-      })
-      this.setState({...newArrayChampions})
-    }
-    getChampions()
-  }
-
-  render(){
-    return (
-      <div className="App">
-        <Routes champions={this.state} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      this app should compile fine
+    </div>
+  );
 }
 
 export default App;
